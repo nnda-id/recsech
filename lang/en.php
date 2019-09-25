89e909f5754def5440a001494988c00ea607ab34
Nanda ID <55502642+nandaid@users.noreply.github.com>
2019-09-25 13:15:16 +0700

    Create en.php

---
+++ lang/en.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['0'] = '[i] Start scanning at';
+$lang['1'] = '[?] SCAN ONLY ';
+$lang['2'] = "[i] Collect domain information ";
+$lang['3'] = "[+] HTTP Headers for Securing :";
+$lang['4'] = "[+] Inactive Domain ";
+$lang['5'] = "[+] Gather information on Github ";
+$lang['6'] = "Disclosure Email on Github";
+$lang['7'] = "[+] Check Honeypot on all domains ";
+$lang['8'] = "[+] IP Based Domain Information :";
+$lang['9'] = "[+] Check Open Port (Port Scanning) ";
+$lang['10'] = "[+] Domain Email";
+$lang['11'] = "[+] Check Subdomain takeover ";
+$lang['12'] = "[+] Check Technologies ";
+$lang['13'] = "[i] Scanning is complete in {h} hour {m} minutes {s} seconds";
+$lang['14'] = "[+] Check Web Application Firewalls (WAF)";
+$lang['15'] = "[+] Search for all (sub) domains";
